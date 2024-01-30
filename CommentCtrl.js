2b4be434ff34689d7c1c11d3a291c68a55146696
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-01-30 23:34:22 +0100

    angular 1er entrainement

---
+++ CommentCtrl.js
@@ -0,0 +1,73 @@
+let component = angular.module('myApp',[]);
+
+component.controller('CommentsCtrl',function($scope){
+    $scope.comments = [{
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    },
+    {
+        "username" : "Maxime",
+        "city" : "Paris",
+        "email" : "maxime.prevot1804kz@gmail.com",
+        "content" : "Salut"
+    }];
+})
+component.controller('usersCtrl',function($scope){
+    $scope.users= [
+    {username:'John',content:'Saluttt'},
+    {username:'Jean',content:'Jean ai rien a faire'},
+    {username:'Paul',content:'Paulitooo'}
+]
+})
+
+
