a6d5783b3ccbce79299c741ea687883b838b5d49
Lirido1404 <102609041+Lirido1404@users.noreply.github.com>
2024-01-31 21:36:03 +0100

    deuxieme commit

--- CommentCtrl.js
+++ CommentCtrl.js
@@ -1,4 +1,22 @@
-let component = angular.module('myApp',[]);
+let component = angular.module('myApp',['ngRoute']);
+
+
+
+component.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller :'CommentsCtrl'
+        })
+        .when('/salut', {
+            templateUrl: 'views/salut.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+
 
 component.controller('CommentsCtrl',function($scope){
     $scope.comments = [{
@@ -69,5 +87,16 @@ component.controller('usersCtrl',function($scope){
     {username:'Paul',content:'Paulitooo'}
 ]
 })
+
+
+component.controller('formvalidcomp', function($scope){
+    $scope.submitForm = function(isValid){
+        if(isValid){
+            alert("Data saved");
+        }
+    }
+});
+
+
 
 
